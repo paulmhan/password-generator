435763ce65bb612f99687c2aa2fe00e827d2a8da
Paul Han <paul.myung.han@gmail.com>
2020-01-11 13:54:43 -0800

    added js

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  copyBtn.removeAttribute("disabled");
+  copyBtn.focus();
+}
+
+function copyToClipboard() {
+  // BONUS 
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
+
+
+// don't need special characters *it's optional
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6,7 ,8 ,9]
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
