aa6e2d5f2bc27c3a7d43e380863cecab1d2202f5
Paul Han <paul.myung.han@gmail.com>
2020-01-15 13:37:22 -0800

    updated

--- script.js
+++ script.js
@@ -27,13 +27,13 @@
 //establish variables
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9]
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let numChar;
 
 
 // prompt user to choose length of password, between 8-128 characters once generate button is clicked
 document.getElementById("generate").addEventListener("click", confirmProperties);
-// document.getElementById("generate").addEventListener("click", generatePassword);
+document.getElementById("generate").addEventListener("click", generatePassword);
 
-let numChar;
 
 
 function confirmProperties() {
@@ -55,11 +55,11 @@ function confirmProperties() {
 
 
 
-// function generatePassword(){
-//     for(i = 0; i <= charLength; i++){
-//         if()
-//     }
-// }
+function generatePassword(){
+    for(i = 0; i <= charLength; i++){
+        if()
+    }
+}
 
 
 
